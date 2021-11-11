f061fd6cdc2bb53300cc818527184613c99338bd
Josebautista10 <cooldj1020@gmail.com>
2021-11-11 14:58:11 -0700

    finished building the main page

--- components/navBar/Intro.js
+++ components/navBar/Intro.js
@@ -4,20 +4,36 @@ function Intro() {
   return (
     <div className='flex flex-col'>
       <div className='flex justify-center flex-col items-center'>
-        <img src='images/me.png' className='w-1/5 rounded-3xl'/>
         <h1 className='text-white py-3 text-3xl'> Hey! I'm Jose Bautista</h1>
+        <img src='images/me.png' className='w-1/5 rounded-3xl' />
         <h2 className='text-white py-3 text-3xl'>
           A Junior front-end developer who loves to learn and build apps with
           JavaScript
         </h2>
       </div>
-      <section className='flex justify-center flex-row space-x-32' >
-      <div >
-        <h1 className='text-white py-3 text-2xl'>How I became a Juniour dev</h1>
-      </div>
-      <div>
-        <h1 className='text-white py-3 text-2xl'>Project management</h1>
-      </div>
+      <section className='flex flex-row justify-around '>
+        <div className='flex flex-col justify-center items-center w-1/2'>
+          <h1 className='text-white py-3 text-2xl flex flex-col justify-center items-center'>
+            How I became a Juniour dev
+          </h1>
+          <a className="contents" target="_blank" href='https://medium.com/@cooldj1020/journey-on-becoming-a-junior-software-developer-6bbab139b7f9'>
+            <img
+              src='images/article.jpeg'
+              className='w-7/12 rounded-3xl '
+            />
+          </a>
+          <p className="text-white py-3 ">This is an artcile I wrote on my journey on becoming a Juniour dev</p>
+        </div>
+        <div className='flex flex-col justify-center items-center w-1/2'>
+          <h1 className='text-white py-3 text-2xl'>Project management App </h1>
+          <a className="contents" target="_blank" href='https://github.com/Josebautista10/WorkLand'>
+            <img
+              src='images/workland.png'
+              className='w-6/12 rounded-3xl '
+            />
+          </a>
+          <p className="text-white py-3 ">Gamified project management app that was built by a team of three </p>
+        </div>
       </section>
     </div>
   )
