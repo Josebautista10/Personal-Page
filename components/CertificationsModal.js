a8729f110b68d4389d533c8c7057c159874365c6
Josebautista10 <cooldj1020@gmail.com>
2021-11-25 23:39:21 -0700

    uploaded certificates

--- components/CertificationsModal.js
+++ components/CertificationsModal.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { AiFillCloseCircle } from 'react-icons/Ai'
+import { AiFillCloseCircle } from 'react-icons/ai'
 // import "./styles.css";
 
 import Modal from 'react-modal'
@@ -32,24 +32,24 @@ function CertificationsModal() {
         <div className='flex justify-center text-3xl'>My certificates</div>
         <div className='bg-red-600 h-full flex items-center justify-around flex-col'>
           <div className='flex-col bg-blue-600 w-1/3'>
-            <img className=' flex justify-center' src='images/portfolio.png' />
+            <img className=' flex justify-center' src='images/certificate/LHL-diploma.png' />
             <p className='flex justify-center'>props.title</p>
           </div>
           <div className='flex flex-wrap justify-around'>
             <Certificate
-              image={'images/portfolio.png'}
+              image={'images/certificate/CSS.png'}
+              title={'FreeCodeCamp Web Development'}
+            />
+            <Certificate
+              image={'images/certificate/JavaScript.png'}
+              title={'FreeCodeCamp JavaScript Algorithms and Data Structure'}
+            />
+            <Certificate
+              image={'images/certificate/wesbos.png'}
               title={'Wes Bos Beginner JavaScript'}
             />
             <Certificate
-              image={'images/portfolio.png'}
-              title={'Wes Bos Beginner JavaScript'}
-            />
-            <Certificate
-              image={'images/portfolio.png'}
-              title={'Wes Bos Beginner JavaScript'}
-            />
-            <Certificate
-              image={'images/portfolio.png'}
+              image={'images/certificate/react-fundamentals.png'}
               title={'Wes Bos Beginner JavaScript'}
             />
           </div>
